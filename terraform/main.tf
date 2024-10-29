2bd33c5846a0f9c57fa42ffb935ee9b755ec196c
Abdurrahman Masood <abdurrahman_masood@hotmail.com>
2024-10-30 00:14:38 +1100

    Added Helm chart deployment code

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,35 +23,10 @@ resource "google_project_service" "kubernetes_engine" {
   disable_on_destroy = true
 }
 
-# resource "google_compute_network" "vpc_network" {
-#   project                 = var.project_id
-#   name                    = var.vpc_network
-#   auto_create_subnetworks = true
-#   depends_on              = [google_project_service.compute_engine]
-# }
-
-resource "google_compute_address" "static_ip" {
-  name         = "my-static-ip"
-  region       = var.region
-  address_type = "EXTERNAL"
-  description  = "Web App Static IP"
-}
-
-output "static_ip_address" {
-  value = google_compute_address.static_ip.address
-}
-
 resource "google_artifact_registry_repository" "my-repo" {
   location      = var.region
   repository_id = var.artifact_registry_repository
   format        = "DOCKER"
-}
-
-
-resource "google_service_account" "default" {
-  account_id   = "service-account-id"
-  display_name = "Service Account"
-  depends_on   = [google_project_service.iam_api]
 }
 
 resource "google_container_cluster" "primary" {
@@ -63,7 +38,7 @@ resource "google_container_cluster" "primary" {
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "my-node-pool"
+  name       = var.gke_cluster_node_pool
   location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = 1
@@ -75,5 +50,4 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
-  depends_on = [google_project_service.iam_api]
 }
--- terraform/variables.tfvars
+++ terraform/variables.tfvars
@@ -3,4 +3,3 @@ region                       = "australia-southeast1"
 artifact_registry_repository = "web-app-docker"
 gke_cluster                  = "web-app-cluster"
 gke_cluster_node_pool        = "web-app-node-pool"
-
