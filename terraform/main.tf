623c821ab3a6958bac96380a1d548fd0c6c631fe
Abdurrahman Masood <abdurrahman_masood@hotmail.com>
2024-10-29 13:37:21 +1100

    Added Terraform GitHub actions workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+resource "google_project_service" "compute_engine" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_compute_network" "vpc_network" {
   project                 = var.project_id
   name                    = var.vpc_network
