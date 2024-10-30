9832201979064500e5c962ec1db609ebf737ec95
Abdurrahman Masood <abdurrahman_masood@hotmail.com>
2024-10-30 14:35:49 +1100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,11 +24,32 @@ resource "google_artifact_registry_repository" "my-repo" {
   format        = "DOCKER"
 }
 
+resource "google_compute_global_address" "static_ip_global" {
+  name = "flask-app-static-ip-global"
+}
+
 # Create a VPC Network
 resource "google_compute_network" "vpc_network" {
   name                    = var.network_name
-  auto_create_subnetworks = true # Disable auto subnet creation
+  auto_create_subnetworks = true
   depends_on              = [google_project_service.compute_engine]
+}
+
+# Create a Cloud Router for the NAT gateway
+resource "google_compute_router" "my_router" {
+  name    = "my-router"
+  region  = var.region
+  network = google_compute_network.vpc_network.name
+}
+
+# Create a NAT gateway
+resource "google_compute_router_nat" "my_nat_gateway" {
+  name   = "my-nat-gateway"
+  region = var.region
+  router = google_compute_router.my_router.name
+
+  nat_ip_allocate_option             = "AUTO_ONLY"                       # Use auto-allocated external IPs
+  source_subnetwork_ip_ranges_to_nat = ["ALL_SUBNETWORKS_ALL_IP_RANGES"] # Enable NAT for all subnets
 }
 
 # # Create Subnets
@@ -39,35 +60,35 @@ resource "google_compute_network" "vpc_network" {
 #   network      = google_compute_network.vpc_network.name
 # }
 
-# Create Firewall Rule to Allow Internal Traffic
-resource "google_compute_firewall" "allow_internal" {
-  name    = "allow-internal"
-  network = google_compute_network.vpc_network.name
+# # Create Firewall Rule to Allow Internal Traffic
+# resource "google_compute_firewall" "allow_internal" {
+#   name    = "allow-internal"
+#   network = google_compute_network.vpc_network.name
 
-  allow {
-    protocol = "tcp"
-    ports    = ["8080"] # Allow all TCP traffic
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["8080"] # Allow all TCP traffic
+#   }
 
-  source_ranges = ["10.0.0.0/24"] # Adjust based on your subnetwork CIDR
-  target_tags   = ["flask-app"]
+#   source_ranges = ["10.0.0.0/24"] # Adjust based on your subnetwork CIDR
+#   target_tags   = ["flask-app"]
 
-}
+# }
 
-# Create Firewall Rule to Allow External Traffic to GKE API
-resource "google_compute_firewall" "allow_gke_api" {
-  name    = "allow-gke-api"
-  network = google_compute_network.vpc_network.name
+# # Create Firewall Rule to Allow External Traffic to GKE API
+# resource "google_compute_firewall" "allow_gke_api" {
+#   name    = "allow-gke-api"
+#   network = google_compute_network.vpc_network.name
 
-  allow {
-    protocol = "tcp"
-    ports    = ["443"] # Allow HTTPS traffic for GKE API
-  }
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["443"] # Allow HTTPS traffic for GKE API
+#   }
 
-  source_ranges = ["0.0.0.0/0"] # Allow access from anywhere (consider restricting this)
-  target_tags   = ["flask-app"]
+#   source_ranges = ["0.0.0.0/0"] # Allow access from anywhere (consider restricting this)
+#   target_tags   = ["flask-app"]
 
-}
+# }
 
 resource "google_container_cluster" "primary" {
   name                     = var.gke_cluster
@@ -75,6 +96,11 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
   network                  = google_compute_network.vpc_network.name
+
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = true
+  }
 
   deletion_protection = false
   depends_on          = [google_project_service.kubernetes_engine]
@@ -94,7 +120,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   node_config {
     preemptible  = true
     machine_type = "e2-medium"
-    tags         = ["flask-app"]
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
