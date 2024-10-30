e185e0ee13b7db8ae809ea9eed7238527e9fa64b
Abdurrahman Masood <abdurrahman_masood@hotmail.com>
2024-10-30 14:54:26 +1100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,19 +25,19 @@ resource "google_artifact_registry_repository" "my-repo" {
 }
 
 resource "google_compute_global_address" "static_ip_global" {
-  name = "flask-app-static-ip-global"
+  name = "flask-app-static-ip"
 }
 
 # Create a VPC Network
 resource "google_compute_network" "vpc_network" {
   name                    = var.network_name
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
   depends_on              = [google_project_service.compute_engine]
 }
 
 # Create a Cloud Router for the NAT gateway
 resource "google_compute_router" "my_router" {
-  name    = "my-router"
+  name    = "my-router123"
   region  = var.region
   network = google_compute_network.vpc_network.name
 }
@@ -52,13 +52,13 @@ resource "google_compute_router_nat" "my_nat_gateway" {
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
-# # Create Subnets
-# resource "google_compute_subnetwork" "subnetwork" {
-#   name          = var.subnetwork_name
-#   ip_cidr_range = "10.0.0.0/24"  # Adjust the CIDR range as needed
-#   region       = var.region    # Must match the region in the provider block
-#   network      = google_compute_network.vpc_network.name
-# }
+# Create Subnets
+resource "google_compute_subnetwork" "subnetwork" {
+  name          = var.subnetwork_name
+  ip_cidr_range = "10.0.0.0/24" # Adjust the CIDR range as needed
+  region        = var.region    # Must match the region in the provider block
+  network       = google_compute_network.vpc_network.name
+}
 
 # # Create Firewall Rule to Allow Internal Traffic
 # resource "google_compute_firewall" "allow_internal" {
@@ -99,6 +99,7 @@ resource "google_container_cluster" "primary" {
 
   private_cluster_config {
     enable_private_nodes    = true
+    master_ipv4_cidr_block  = "10.0.1.0/28" # Reserved for GKE master endpoint
     enable_private_endpoint = true
   }
 
